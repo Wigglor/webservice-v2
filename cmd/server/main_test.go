673331a011487388a3240d079957c5f0bc55bcf2
Notna <anton.vigstrand@gmail.com>
2024-10-18 16:25:22 +0200

    changed test again

--- cmd/server/main_test.go
+++ cmd/server/main_test.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"reflect"
@@ -39,54 +38,6 @@ func TestGetUsers(t *testing.T) {
 	req, _ := http.NewRequest("GET", "/users", nil)
 	handler := router.UserHandler{Repo: &mockUserModel{}}
 	http.HandlerFunc(handler.GetUsers).ServeHTTP(rec, req)
-	fmt.Println(rec.Body.String())
-	expected := []repository.User{
-		{
-			ID:                 1,
-			Name:               "Joe Doe",
-			Email:              "johndoe@email.com",
-			SubID:              "subid_123abc",
-			VerificationStatus: true,
-			SetupStatus:        "pending",
-			CreatedAt:          pgtype.Timestamptz{Valid: false},
-			UpdatedAt:          pgtype.Timestamptz{Valid: false},
-		},
-		{
-			ID:                 2,
-			Name:               "Jane Doe",
-			Email:              "janedoe@email.com",
-			SubID:              "subid_456def",
-			VerificationStatus: true,
-			SetupStatus:        "pending",
-			CreatedAt:          pgtype.Timestamptz{Valid: false},
-			UpdatedAt:          pgtype.Timestamptz{Valid: false},
-		},
-	}
-	var obtained []repository.User
-	err := json.Unmarshal(rec.Body.Bytes(), &obtained)
-	if err != nil {
-		t.Fatalf("Failed to unmarshal response body: %v", err)
-	}
-
-	// Compare the expected and obtained data
-	if !reflect.DeepEqual(expected, obtained) {
-		t.Errorf("\n...expected = %v\n...obtained = %v", expected, obtained)
-	}
-
-	// Alternatively, using testify's assert
-	assert.Equal(t, expected, obtained, "The expected and obtained users should be equal")
-}
-
-/*
-func TestGetUsers(t *testing.T) {
-	rec := httptest.NewRecorder()
-	req, _ := http.NewRequest("GET", "/users", nil)
-	userRepo := repository.NewUserRepository(&mockUserModel{})
-	// env := Env{users: &mockUserModel{}, context: context.Background()}
-	env := repository.UserRepo{db: &mockUserModel{}}
-
-	http.HandlerFunc(env.GetUsers).ServeHTTP(rec, req)
-	fmt.Println(rec.Body.String())
 	expected := []repository.User{
 		{
 			ID:                 1,
@@ -123,7 +74,6 @@ func TestGetUsers(t *testing.T) {
 	// Alternatively, using testify's assert
 	assert.Equal(t, expected, obtained, "The expected and obtained users should be equal")
 	// if expected != rec.Body.String() {
-	// 	t.Errorf("\n...expected = %v\n...obtained = %v", expected, rec.Body.String())
-	// }
-
-}*/
+	// 		t.Errorf("\n...expected = %v\n...obtained = %v", expected, rec.Body.String())
+	// 	}
+}
