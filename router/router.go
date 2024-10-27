7e82f499f4cf2c54e89937d4e615710cb2ef37c1
Notna <anton.vigstrand@gmail.com>
2024-10-27 16:18:05 +0100

    changed from chi router

--- router/router.go
+++ router/router.go
@@ -2,20 +2,48 @@ package router
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+
+	"net/url"
+	"time"
 
 	"github.com/Wigglor/webservice-v2/handlers"
 	"github.com/Wigglor/webservice-v2/middlewares"
-	"github.com/go-chi/chi/v5"
-	chi_middleware "github.com/go-chi/chi/v5/middleware"
-	"github.com/go-chi/cors"
+	jwtmiddleware "github.com/auth0/go-jwt-middleware/v2"
+	"github.com/auth0/go-jwt-middleware/v2/jwks"
+	"github.com/auth0/go-jwt-middleware/v2/validator"
+	// "github.com/go-chi/chi/v5"
+	// chi_middleware "github.com/go-chi/chi/v5/middleware"
+	// "github.com/go-chi/cors"
 )
 
 // func Routes(handler *UserHandler) http.Handler {
 func Routes(handler *handlers.UserHandler) http.Handler {
-	router := chi.NewRouter()
+
+	// finalHandler := http.HandlerFunc(helloAuth)
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /api/users", handler.GetUsers)
+	mux.HandleFunc("GET /api/user/{id}", handler.GetUserById)
+	// mux.HandleFunc("GET /api/auth/protected", middlewares.EnsureValidToken(finalHandler))
+	// This route is only accessible if the user has a valid access_token.
+	mux.Handle("/api/private", middlewares.EnsureValidToken()(
+		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			// CORS Headers.
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
+			w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+			w.Header().Set("Access-Control-Allow-Headers", "Authorization")
+
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte(`{"message":"Hello from a private endpoint! You need to be authenticated to see this."}`))
+		}),
+	))
+	return mux
+	/*router := chi.NewRouter()
 	router.Use(chi_middleware.Recoverer)
 	router.Use(chi_middleware.Logger)
+	router.Use(ValidateJWT)
 	router.Use(middlewares.LoggerMiddleware)
 	router.Use(cors.Handler(cors.Options{
 		AllowedOrigins:   []string{"http://*", "https://*"},
@@ -26,13 +54,18 @@ func Routes(handler *handlers.UserHandler) http.Handler {
 		MaxAge:           300,
 	}))
 
+	router.Get("/protected", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Access granted to protected route!"))
+	})
+
 	router.Route("/api", func(r chi.Router) {
 		r.Get("/users", handler.GetUsers)
 		r.Get("/user/{id}", handler.GetUserById)
-		// r.With().Get("/protected", helloAuth)
+		r.With().Get("/protected", helloAuth)
 
 		r.Route("/auth", func(r chi.Router) {
-			r.Use(middlewares.EnsureValidToken)
+			// r.Use(middlewares.EnsureValidToken)
+			// r.Use(middlewares.ValidateJWT)
 			//r.With(middlewares.EnsureValidToken).Get("/protected", helloAuth)
 			// r.Use(middlewares.EnsureValidToken())
 			r.Get("/", helloWorld) // DELETE /articles/123
@@ -40,7 +73,7 @@ func Routes(handler *handlers.UserHandler) http.Handler {
 
 	})
 
-	return router
+	return router*/
 }
 
 func helloWorld(w http.ResponseWriter, r *http.Request) {
@@ -48,7 +81,54 @@ func helloWorld(w http.ResponseWriter, r *http.Request) {
 }
 
 func helloAuth(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello there, World!")
+	// fmt.Fprintf(w, "Hello there, World!")
+	w.Write([]byte("OK"))
+}
+
+func ValidateJWT(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		issuerURL, err := url.Parse("https://" + "dev-frhs26nrb6ju4iai.us.auth0.com" + "/")
+		if err != nil {
+			log.Fatalf("Failed to parse the issuer url: %v", err)
+		}
+
+		provider := jwks.NewCachingProvider(issuerURL, 5*time.Minute)
+
+		jwtValidator, err := validator.New(
+			provider.KeyFunc,
+			validator.RS256,
+			issuerURL.String(),
+			[]string{"https://hello-world.example.com/api/v2/"},
+		)
+		if err != nil {
+			log.Fatalf("Failed to set up the jwt validator")
+		}
+
+		errorHandler := func(w http.ResponseWriter, r *http.Request, err error) {
+			log.Printf("Encountered error while validating JWT: %v", err)
+
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusUnauthorized)
+			w.Write([]byte(`{"message":"Failed to validate JWT."}`))
+		}
+
+		middleware := jwtmiddleware.New(
+			jwtValidator.ValidateToken,
+			jwtmiddleware.WithErrorHandler(errorHandler),
+		)
+
+		// Log authorization header for debugging
+		authHeader := r.Header.Get("Authorization")
+		log.Printf("Authorization Header: %s", authHeader)
+
+		// Pass through the middleware, which may modify the request context
+		middleware.CheckJWT(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			log.Println("Token validated successfully.")
+			next.ServeHTTP(w, r)
+		})).ServeHTTP(w, r)
+
+		middleware.CheckJWT(next).ServeHTTP(w, r)
+	})
 }
 
 /*type UserHandler struct {
